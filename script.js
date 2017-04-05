c954bc5bd71d2fda00a42fac5f80936369196b3f
threemusketeerz <work.bjarke@gmail.com>
2017-04-05 13:09:20 +0200

    Managed to restrict controls, restricting opposite direction of current trajectory.

--- script.js
+++ script.js
@@ -7,19 +7,23 @@ fH = 70;
 fW = 70;
 posx = w/2;
 posy = h/2;
+var gameOver = false;
 
 function preload() {
     fruit = loadImage("images/fruit.png");
 }
 
 function setup() {
+    frameRate(10);
     createCanvas(w, h);
     fruitx = floor(random(0, w - 50));
     fruity = floor(random(0, h - 50));
-    console.log("fruit: " + fruitx +" "+ fruity);
+    noLoop();
+    // console.log("fruit: " + fruitx +" "+ fruity);
 }
 
 function draw() {
+
     background(0);
 
     image(fruit, fruitx, fruity, fW, fH);
@@ -28,5 +32,18 @@ function draw() {
     ellipse(posx, posy, 20);
     ellipseMode(CENTER);
 
+
     snake();
+    // controls();
+}
+
+
+function keyPressed() {
+    if (keyCode === 88) {
+        noLoop();
+    }
+    if (keyCode === 67) {
+        loop();
+    }
+    return false;
 }
--- snake.js
+++ snake.js
@@ -1,51 +1,91 @@
-var tLength, tailx, taily;
-tLength = 4;
+var tLength, tailx, taily, vel;
+tLength = 0;
 var tailArr = [];
+var contr = {
+    up: false,
+    left: false,
+    right: false,
+    down: false
+};
 
 function snake () {
+    vel = 2;
     tailArr.unshift([posx, posy]);
-    tailArr = tailArr.slice(0, tLength - 1);
-    beginShape();
-    for (var i = 0; i < tailArr.length; i++) {
-        // for (var j = 0; j < tailArr[i].length -1; j++) {
-        for(var j = 0; j < tailArr[i].length; j++){
-            
-        }
+    tailArr = tailArr.slice(0, (tLength - 1));
+    for (var i = 0; i < tailArr.length - 1; i++) {
+        fP = tailArr[i][0];
+        sP = tailArr[i][1];
+        fP2 = tailArr[i+1][0];
+        sP2 = tailArr[i+1][1];
+        line(fP, sP, fP2, sP2);
+        stroke(255);
+        strokeWeight(10);
     }
-    endShape();
-
-    if (keyIsPressed === true) {
-        if (keyCode === UP_ARROW){
-            posy -= 2;
-        }
-        if (keyCode === DOWN_ARROW){
-            posy += 2;
-        }
-        if (keyCode === LEFT_ARROW){
-            posx -= 2;
-        }
-        if (keyCode === RIGHT_ARROW){
-            posx += 2;
-        }
-    }
+    strokeWeight(1);
+    // }
     if (posx >= fruitx - 12 && posx <= fruitx + 12 && posy >= fruity - 12 && posy <= fruity + 12) {
-        tLength++;
+        tLength += 10;
         fruitx = floor(random(0, w - 50));
         fruity = floor(random(0, h - 50));
         image(fruit, fruitx, fruity, fW, fH);
     }
-    if(posx === w) posx = 0;
-    else if (posx === 0) posx = w;
-    if (posy === h) posy = 0;
-    else if (posy === 0) posy = h;
-    posy++;
-    // for (var i = 1; i < tLength; i++) {
-    //     // tailArr[0] = [posx, posy];
-    //     tailArr.push(tailArr[i - 1]);
-    //     // for (var j = 1; j < tailArr.length; j++) {
-    //     //     tailArr.push(tailArr[j - 1]);
-    //     // }
-    // }
-    console.log("Pos: " + posx, posy);
-    console.log("tail: ", tailArr);
+
+    //Can make for loop to only replace either y or x to 0 or w.
+    if(posx === w) {posx = 0; tailArr = [];}
+    else if (posx === 0) {posx = w; tailArr = [];}
+    if (posy === h) {
+        posy = 0; 
+        tailArr = [];
+    }
+    else if (posy === 0) {posy = h; tailArr = [];}
+
+    controls();
+    // console.log("Pos: " + posx, posy);
+    // console.log("tail: ", tailArr);
+}
+
+function controls() {
+
+    //using object to manage controls, this limits the controls ex. up can't go down.
+    if (keyCode === UP_ARROW && contr.down === false) {
+        console.log("up");
+        contr.up = true;
+        contr.left = false;
+        contr.right = false;
+        contr.down = false;
+    }
+    if (keyCode === LEFT_ARROW && contr.right === false){
+        console.log("left");
+        contr.up = false;
+        contr.left = true;
+        contr.right = false;
+        contr.down = false;
+    }
+    if (keyCode === RIGHT_ARROW && contr.left === false){
+        console.log("right");
+        contr.up = false;
+        contr.left = false;
+        contr.right = true;
+        contr.down = false;
+    }
+    if (keyCode === DOWN_ARROW && contr.up === false) {
+        console.log("down");
+        contr.up = false;
+        contr.left = false;
+        contr.right = false;
+        contr.down = true;
+    }
+
+    if (contr.up === true) {
+        posy -= vel;
+    }
+    if (contr.left === true) {
+        posx -= vel;
+    }
+    if (contr.right === true) {
+        posx += vel;
+    }
+    if (contr.down === true) {
+        posy += vel;
+    }
 }
